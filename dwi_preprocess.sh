31be0790dab83b618ccdeabdc0f9875aa2b66c57
Benjamin Zhang <bnzhang2001@gmail.com>
2024-09-27 20:03:55 -0700

    Update dwi_preprocess.sh

--- dwi_preprocess.sh
+++ dwi_preprocess.sh
@@ -6,28 +6,29 @@ for i in 001 #002 003 004 005 006 007 008 009 010 011 012 013 014 015 016 017 01
 
 do
  
-	# Convert dwi.nrrd to dwi.nifti so that it can be input into mrconvert to get a .mif file
  echo "${i} converting nrrd to nifti"
  
-# nifti_write.py -i ${i}/DTIPrep/dwi.nrrd
+	nifti_write.py -i ${i}/DTIPrep/dwi.nrrd
  
  echo "${i} Converting to .mif"
  
- #mrconvert ./${i}/DTIPrep/dwi.nii.gz ./${i}/DTIPrep/dwi.mif -fslgrad ./${i}/DTIPrep/bvec ./${i}/DTIPrep/bval
+ 	mrconvert ./${i}/DTIPrep/dwi.nii.gz ./${i}/DTIPrep/dwi.mif -fslgrad ./${i}/DTIPrep/bvec ./${i}/DTIPrep/bval
  
  echo "${i} Denoising"
  
- #dwidenoise ./${i}/DTIPrep/dwi.mif ./${i}/DTIPrep/dwi_denoised.mif
+ 	dwidenoise ./${i}/DTIPrep/dwi.mif ./${i}/DTIPrep/dwi_denoised.mif
  
  
  echo "${i} Degibbsing"
  
-# mrdegibbs ./${i}/DTIPrep/dwi_denoised.mif ./${i}/DTIPrep/dwi_denoised_degibbs.mif -axes 0,1
+	mrdegibbs ./${i}/DTIPrep/dwi_denoised.mif ./${i}/DTIPrep/dwi_denoised_degibbs.mif -axes 0,1
  
- 
- 
- dwifslpreproc ./${i}/DTIPrep/dwi_denoised_degibbs.mif ./${i}/DTIPrep/dwi_denoised_degibbs_preproc.mif -rpe_none -pe_dir AP
- 
- dwibiascorrect ant ./${i}/DTIPrep/dwi_denoised_degibbs_preproc.mif ./${i}/DTIPrep/dwi_denoised_degibbs_preproc_unbiased.mif
+ echo "${i} Motion and distortion correction"
+	
+  	dwifslpreproc ./${i}/DTIPrep/dwi_denoised_degibbs.mif ./${i}/DTIPrep/dwi_denoised_degibbs_preproc.mif -rpe_none -pe_dir AP
+
+ echo "${i} Bias field correction"
+
+  	dwibiascorrect ant ./${i}/DTIPrep/dwi_denoised_degibbs_preproc.mif ./${i}/DTIPrep/dwi_denoised_degibbs_preproc_unbiased.mif
  
  done
