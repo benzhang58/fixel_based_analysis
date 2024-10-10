406cf47ffe6a3c98bd6f24f8ca347e0d5d8f4650
Benjamin Zhang <bnzhang2001@gmail.com>
2024-10-10 16:46:53 -0700

    Update dwi_preprocess.sh

--- dwi_preprocess.sh
+++ dwi_preprocess.sh
@@ -1,34 +1,47 @@
-cd /Users/ccri/Desktop/Data_Analysis/MotorLearning
-source /Users/ccri/miniconda3/bin/activate
+cd /Volumes/exhale/rocketship
 
 
-for i in 001 #002 003 004 005 006 007 008 009 010 011 012 013 014 015 016 017 018 019 020 021 022 023 024 025 026 027 028 029 030 031 032 033 034 035 036 037 038 039 040 041 042 043 044 045 046 047 048 049 050 
+for i in  001 002 003 004 005 007 008 009 010 011 012 014 015 016 017 019 020 021 022 023 024 026 027 028 029 030 031 032 033 034 035 036 037 038 039 040 041 042 043 044 045 046 047 048 049 050
 
 do
- 
- echo "${i} converting nrrd to nifti"
- 
-	nifti_write.py -i ${i}/DTIPrep/dwi.nrrd
- 
- echo "${i} Converting to .mif"
- 
- 	mrconvert ./${i}/DTIPrep/dwi.nii.gz ./${i}/DTIPrep/dwi.mif -fslgrad ./${i}/DTIPrep/bvec ./${i}/DTIPrep/bval
- 
- echo "${i} Denoising"
- 
- 	dwidenoise ./${i}/DTIPrep/dwi.mif ./${i}/DTIPrep/dwi_denoised.mif
- 
- 
- echo "${i} Degibbsing"
- 
-	mrdegibbs ./${i}/DTIPrep/dwi_denoised.mif ./${i}/DTIPrep/dwi_denoised_degibbs.mif -axes 0,1
- 
- echo "${i} Motion and distortion correction"
 	
-  	dwifslpreproc ./${i}/DTIPrep/dwi_denoised_degibbs.mif ./${i}/DTIPrep/dwi_denoised_degibbs_preproc.mif -rpe_none -pe_dir AP
-
- echo "${i} Bias field correction"
-
-  	dwibiascorrect ants ./${i}/DTIPrep/dwi_denoised_degibbs_preproc.mif ./${i}/DTIPrep/dwi_denoised_degibbs_preproc_unbiased.mif
+mkdir -p ./${i}/Fixel_Analysis
  
+# Iterate through  files in that order and use the first one that exists
+
+for filename in ./${i}/*64DIFF*003a001.nii.gz ./${i}/*64DIFF*004a001.nii.gz ./${i}/*64DIFF*005a001.nii.gz
+do
+    if [ -f "$filename" ]; then  # Check if the file exists
+        echo "Using file: $filename"
+
+        echo "${i} Converting to .mif"
+        mrconvert "$filename" ./${i}/Fixel_Analysis/dwi.mif -fslgrad ./${i}/*64DIFF1000RESEARCH2s*.bvec ./${i}/*64DIFF1000RESEARCH2s*.bval
+
+        break  # Break the loop after finding the first valid file
+    fi
+done
+
+
+# Denoising and Gibbs ringing removal 
+echo "${i} Denoising"
+ 
+dwidenoise ./${i}/Fixel_Analysis/dwi.mif ./${i}/Fixel_Analysis/dwi_denoised.mif
+ 
+echo "${i} Degibbsing"
+ 
+mrdegibbs ./${i}/Fixel_Analysis/dwi_denoised.mif ./${i}/Fixel_Analysis/dwi_denoised_degibbs.mif -axes 0,1
+
+# Motion and distortion correction - including eddy current distortions
+echo "${i} Preprocessing"
+
+dwifslpreproc ./${i}/dwi_denoised_degibbs.mif ./${i}/Fixel_Analysis/dwi_denoise_degibbs_preproc.mif -rpe_none -pe_dir AP
+
+# Bias field correction to improve brain mask estimation at later step 
+echo "${i} Bias Correcting"
+
+dwibiascorrect ant ./${i}/DTIPrep/dwi_denoised_degibbs_preproc.mif ./${i}/DTIPrep/dwi_denoised_degibbs_preproc_unbiased.
+
+        
+		
  done
+ 
