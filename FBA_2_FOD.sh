0b120d1a0814f6be6a09059c2bf1ad620bc0ccb4
Benjamin Zhang <bnzhang2001@gmail.com>
2024-10-10 16:49:35 -0700

    Update FBA_2_FOD.sh

--- FBA_2_FOD.sh
+++ FBA_2_FOD.sh
@@ -1,54 +1,25 @@
-cd /Users/ccri/Desktop/Raw_Data/MRI/MotorLearning
+cd /Users/ccri/Desktop/Data_Analysis/MotorLearning
 
-
-for i in  045 #042 043 044 045 046 047 048 049 050 #001 skip  because already done 002 003 004 005 007 008 009 010 011 012 014 015 016 017 019 020 021 022 023 024 026 027 028 029 030 031 032 033 034 035 036 037 038 039 040 041 
-
+for i in 001 002 003 004 005 006 007 008 009 010 011 012 013 014 015 016 017 018 019 020 021 022 023 024 025 026 027 028 029 030 031 032 033 034 035 036 037 038 039 040 041 042 043 044 045 046 047 048 049 050 
 do
-	
-	mkdir -p ./${i}/Fixel_Analysis
- 
-# Convert dwi.nrrd to dwi.nifti so that it can be input into mrconvert to get a .mif file
- #echo "${i} converting nrrd to nifti"
- 
-# nifti_write.py -i ${i}/DTIPrep/dwi.nrrd
- 
- 
- 
-# Iterate through 003, 004, and 005 files in that order and use the first one that exists
-for filename in ./${i}/*64DIFF*003a001.nii.gz ./${i}/*64DIFF*004a001.nii.gz ./${i}/*64DIFF*005a001.nii.gz
-do
-    if [ -f "$filename" ]; then  # Check if the file exists
-        #echo "Using file: $filename"
 
-       # echo "${i} Converting to .mif"
-        # mrconvert "$filename" ./${i}/Fixel_Analysis/dwi.mif -fslgrad ./${i}/*64DIFF1000RESEARCH2s*.bvec ./${i}/*64DIFF1000RESEARCH2s*.bval
+echo "Participant ${i}"
 
-        break  # Break the loop after finding the first valid file
-    fi
+echo "Upsampling DWI Data"
+# Upsample DWI data to isotropic voxel size of 1.25 mm 
+mrgrid ./${i}/Fixel_analysis/dwi_denoised_degibbs_preproc_unbiased.mif regrid -vox 1.25 ./${i}/Fixel_analysis/dwi_denoised_degibbs_preproc_unbiased_upsampled.mif
+
+echo "Computing brain mask"
+# Compute brain mask from upsampled data 
+dwi2mask ./${i}/Fixel_analysis/dwi_denoised_degibbs_preproc_unbiased_upsampled.mif ./${i}/Fixel_analysis/dwi_upsampled_mask.mif
+
+echo "Estimating FOD"
+# Fibre Orientation Distribution estimation (multi-tissue spherical deconvolution) with brain mask --> only get WM and CSF, since we only have two bvals (0 and 1000)
+dwi2fod msmt_csd ./${i}/Fixel_analysis/dwi_denoised_degibbs_preproc_unbiased_upsampled.mif ./group_average_response_wm.txt ./${i}/wmfod.mif ./group_average_response_csf.txt ./${i}/csf.mif -mask ./${i}/DTIPrep/dwi_upsampled_mask.mif
+
+echo "Bias field correction and intensity normalization"
+# Joint bias field correction and intensity normalisation
+mtnormalise ./${i}/wmfod.mif ./${i}/wmfod_norm.mif ./${i}/csf.mif ./${i}/csf_norm.mif -mask ./${i}/DTIPrep/dwi_upsampled_mask.mif
+
 done
 
-
- 
- echo "${i} Denoising"
- 
-# dwidenoise ./${i}/Fixel_Analysis/dwi.mif ./${i}/Fixel_Analysis/dwi_denoised.mif -force
- 
- 
- echo "${i} Degibbsing"
- 
-mrdegibbs ./${i}/Fixel_Analysis/dwi_denoised.mif ./${i}/Fixel_Analysis/dwi_denoised_degibbs.mif -axes 0,1
- 
- 
-# dwifslpreproc ./${i}/DTIPrep/dwi_denoised_degibbs.mif ./${i}/DTIPrep/dwi_denoised_degibbs_preproc.mif -rpe_none -pe_dir AP
- 
-# dwibiascorrect ant ./${i}/DTIPrep/dwi_denoised_degibbs_preproc.mif ./${i}/DTIPrep/dwi_denoised_degibbs_preproc_unbiased.
-
-
-
-# Break the loop after finding the first valid file
-        
-		
-	
- done
- 
- 
